e801ed56180f5e171314f4653ea947c4d2047e02
David Grammatico <david.lugdunum@gmail.com>
2021-03-09 14:11:27 +0100

    validation models

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,8 @@
 class Bike < ApplicationRecord
   belongs_to :user
+  validates :description, :price, :name, :photo, presence: true
+  validates :price, numericality: true
+  validates :price, numericality: { only_integer: true }
 end
+
+
