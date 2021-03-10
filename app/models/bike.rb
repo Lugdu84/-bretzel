b2f2ebf29ca6c2d72c086b9f0446e5965ae744a1
David Grammatico <david.lugdunum@gmail.com>
2021-03-10 21:04:04 +0100

    model favorites

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,6 +2,7 @@ class Bike < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
+  has_many :favorites
   validates :description, :price, :name, :photo, presence: true
   # validates :price, numericality: true
   # validates :price, numericality: { only_integer: true }
