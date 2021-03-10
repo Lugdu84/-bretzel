422145a22d4dff0d3e046de99b3e5679300ea2cf
David Grammatico <david.lugdunum@gmail.com>
2021-03-10 09:13:50 +0100

    markers personnalized

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -4,7 +4,12 @@ class BikesController < ApplicationController
   def index
     @bikes = Bike.where.not(latitude: nil, longitude: nil)
     @markers = @bikes.map do |bike|
-      {lat: bike.latitude, lng: bike.longitude}
+      {
+        lat: bike.latitude,
+        lng: bike.longitude,
+        infoWindow: render_to_string(partial: "infowindow", locals: { bike: bike }),
+        image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+      }
     end
   end
 
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,8 +5,18 @@ const mapElement = document.getElementById('map');
 
 const addMarkers = (map, markers) => {
     markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+        // Create a HTML element for your custom marker
+        const element = document.createElement('div');
+        element.className = 'marker';
+        element.style.backgroundImage = `url('${marker.image_url}')`;
+        element.style.backgroundSize = 'contain';
+        element.style.width = '25px';
+        element.style.height = '25px';
         new mapboxgl.Marker()
             .setLngLat([ marker.lng, marker.lat ])
+            .setPopup(popup)
             .addTo(map);
     });
 }
