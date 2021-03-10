0235439b8f8abaf193dae8c231b436070564bae8
David Grammatico <david.lugdunum@gmail.com>
2021-03-10 21:09:22 +0100

    model review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_203806) do
+ActiveRecord::Schema.define(version: 2021_03_10_200606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,24 @@ ActiveRecord::Schema.define(version: 2021_03_09_203806) do
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "bike_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bike_id"], name: "index_favorites_on_bike_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "note"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,4 +60,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_203806) do
   end
 
   add_foreign_key "bikes", "users"
+  add_foreign_key "favorites", "bikes"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "reviews", "users"
 end
