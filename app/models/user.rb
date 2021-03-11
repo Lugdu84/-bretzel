e5152e3cd845d14ea549139654685045200fa61b
David Grammatico <david.lugdunum@gmail.com>
2021-03-11 09:09:41 +0100

    through bikes in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :bikes
   has_many :favorites
+  # to access bikes
+  has_many :bikes, through: :favorites
   has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
