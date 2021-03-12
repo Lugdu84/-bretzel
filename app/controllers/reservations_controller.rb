d1ec976c32dd9b9eaa7a58fdea325533bede81a7
Julien FEISTHAUER <julien.feisthauer@gmail.com>
2021-03-12 11:10:47 +0100

    front reservations P4

--- app/controllers/reservations_controller.rb
+++
@@ -1,6 +0,0 @@
-class ReservationsController < ApplicationController
-  skip_before_action :authenticate_user!
-  def index
-  end
-end
-# a discuter with team, obliger le login AVANT mm de cliquer sur resa ou plus tard
